c30411108c2d5a538eefb7ceb76c07594f359184
juanmiloz <juanmiloz@hotmail.com>
2025-01-06 12:26:53 -0500

    feat(repositories): add ProductRepository interface for product data operations
    - Created ProductRepository interface extending JpaRepository.
    - Configured the repository to manage Product entities with a primary key of type Long.
    - Provides built-in CRUD functionality and query support for the Product entity.

--- src/main/java/com/microcore/springcloud/msvc/products/repositories/ProductRepository.java
+++ src/main/java/com/microcore/springcloud/msvc/products/repositories/ProductRepository.java
@@ -1,4 +1,8 @@
 package com.microcore.springcloud.msvc.products.repositories;
 
-public interface ProductRepository {
+import com.microcore.springcloud.msvc.products.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
 }
