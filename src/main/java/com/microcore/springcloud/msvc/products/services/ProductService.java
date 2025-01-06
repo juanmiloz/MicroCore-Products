f63d68ca667cabbe0054e795bdb43414d20125db
juanmiloz <juanmiloz@hotmail.com>
2025-01-06 12:28:53 -0500

    feat(services): add ProductService interface and its implementation ProductServiceImpl
    - Created ProductService interface to define contract methods for product management:
      - indAll(): Retrieves all products.
      - indById(Long id): Retrieves a product by its ID.
    
    - Implemented ProductService with ProductServiceImpl:
      - Annotated with @Service to handle business logic for products.
      - Used Lombok's @AllArgsConstructor to simplify constructor injection of ProductRepository.
      - Implemented methods:
        - indAll(): Fetches all products using ProductRepository.
        - indById(Long id): Retrieves a product by its ID with Optional handling.
      - Added @Transactional(readOnly = true) for efficient read-only database operations.
    
    This commit sets up the service layer for managing product data and ensures separation of concerns between the interface and its implementation.

--- src/main/java/com/microcore/springcloud/msvc/products/services/ProductService.java
+++ src/main/java/com/microcore/springcloud/msvc/products/services/ProductService.java
@@ -1,4 +1,13 @@
 package com.microcore.springcloud.msvc.products.services;
 
-public class ProductService {
+import com.microcore.springcloud.msvc.products.entities.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductService {
+
+    List<Product> findAll();
+    Optional<Product> findById(Long id);
+
 }
--- src/main/java/com/microcore/springcloud/msvc/products/services/ProductServiceImpl.java
+++ src/main/java/com/microcore/springcloud/msvc/products/services/ProductServiceImpl.java
@@ -1,4 +1,29 @@
 package com.microcore.springcloud.msvc.products.services;
 
-public class ProductServiceImpl {
+import com.microcore.springcloud.msvc.products.entities.Product;
+import com.microcore.springcloud.msvc.products.repositories.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class ProductServiceImpl implements ProductService {
+
+    final private ProductRepository productRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
 }
