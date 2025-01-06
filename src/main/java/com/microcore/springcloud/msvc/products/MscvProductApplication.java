fc461869c01872d36e81147741411f5e37feb411
juanmiloz <juanmiloz@hotmail.com>
2025-01-06 11:52:11 -0500

    base commit - for products microservice

---
+++ src/main/java/com/microcore/springcloud/msvc/products/MscvProductApplication.java
@@ -0,0 +1,13 @@
+package com.microcore.springcloud.msvc.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscvProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MscvProductApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microcore/springcloud/msvc/products/MscvProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microcore.springcloud.msvc.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscvProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
