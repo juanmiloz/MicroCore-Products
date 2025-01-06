b29f5d4aa4e61f676499e449beae1e139effe916
juanmiloz <juanmiloz@hotmail.com>
2025-01-06 12:41:25 -0500

    feat(controller): add ProductController for managing product endpoints
    - Created ProductController annotated with @RestController to handle HTTP requests.
    - Used Lombok's @AllArgsConstructor to simplify dependency injection of ProductService.
    - Added endpoints:
      - GET /: Fetches a list of all products and returns it as ResponseEntity.
      - GET /{id}: Fetches details of a specific product by its ID.
        - Returns HTTP 200 (OK) with the product if found.
        - Returns HTTP 404 (Not Found) if the product does not exist.
    
    This commit introduces the controller layer for handling client requests related to products and integrates it with the service layer.

--- src/main/java/com/microcore/springcloud/msvc/products/controllers/ProductController.java
+++ src/main/java/com/microcore/springcloud/msvc/products/controllers/ProductController.java
@@ -1,4 +1,38 @@
 package com.microcore.springcloud.msvc.products.controllers;
 
+import com.microcore.springcloud.msvc.products.entities.Product;
+import com.microcore.springcloud.msvc.products.services.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@AllArgsConstructor
 public class ProductController {
+
+    final ProductService productService;
+
+    @GetMapping()
+    public ResponseEntity<List<Product>> list() { //In the response entity we can put ? to return any type of object
+        return ResponseEntity.ok(productService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> details(@PathVariable Long id) {
+        Optional<Product> productOptional = productService.findById(id);
+
+        if(productOptional.isPresent()) {
+            return ResponseEntity.ok(productOptional.orElseThrow());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+
+
+    }
+
 }
